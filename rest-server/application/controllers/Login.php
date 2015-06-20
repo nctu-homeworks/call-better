6ab9b442ffdd30fd16aacba46c60aadf6cbd3a3c
Chih-Yung Liang <stu89424@gmail.com>
2015-06-20 23:28:00 +0800

    Register API (login/register).

--- rest-server/application/controllers/Login.php
+++ rest-server/application/controllers/Login.php
@@ -6,7 +6,7 @@ class Login extends REST_Controller
 {
 	protected $methods = array(
 		'login_post' => array(),
-		'register_pos' => array()
+		'register_post' => array()
 	);
 
 	public function login_post() {
@@ -21,6 +21,47 @@ class Login extends REST_Controller
 
 		$this->load->model("Session_model");
 		$this->Session_model->login_session($this->_apiuser->id, $login_user['id']);
-		$this->response($result, 200);
+		$this->response($login_user, 200);
+	}
+
+	public function register_post() {
+		$arg_keys = array('name', 'sex', 'birthday', 'email', 'password');
+		$args = array_combine($arg_keys, array_map(array($this, 'post'), $arg_keys));
+
+		$bad_args = array();
+		if (!$args['name'])
+			$bad_args[] = 'name';
+
+		if ($args['sex'] === FALSE || !in_array($args['sex'], array('0', '1'), TRUE))
+			$bad_args[] = 'sex';
+
+		if (!$args['birthday'])
+			$bad_args[] = 'birthday';
+		else {
+			$d = DateTime::createFromFormat('Y-m-d', $args['birthday']);
+			if (!$d || $d->format('Y-m-d') != $args['birthday'])
+				$bad_args[] = 'birthday';
+		}
+
+		if (!$args['email'] || !filter_var($args['email'], FILTER_VALIDATE_EMAIL))
+			$bad_args[] = 'email';
+
+		if (!$args['password'])
+			$bad_args[] = 'password';
+
+		if (!empty($bad_args))
+			$this->response(array('error' => 'Arguments not valid.', 'bad' => $bad_args), 400);
+
+
+		$this->load->model("Account_model");
+		$register_user = $this->Account_model->register($args);
+		if (!$register_user)
+			$this->response(array('error' => 'Cannot create user.'), 500);
+		else if ($register_user == 'duplicated')
+			$this->response(array('error' => 'Email duplicated.'), 409);
+
+		$this->load->model("Session_model");
+		$this->Session_model->login_session($this->_apiuser->id, $register_user['id']);
+		$this->response($register_user, 201);
 	}
 }
--- rest-server/application/models/Account_model.php
+++ rest-server/application/models/Account_model.php
@@ -13,4 +13,27 @@ class Account_model extends CI_Model
 
 		return array_intersect_key($user, array_flip(array('id', 'name', 'email', 'sex', 'birthday')));
 	}
+
+	public function register($data) {
+		$pwd_hash = password_hash($data['password'], PASSWORD_DEFAULT);
+		if (!$pwd_hash)
+			return FALSE;
+
+		$user_data = array(
+			'name' => $data['name'],
+			'birthday' => $data['birthday'],
+			'sex' => $data['sex'],
+			'register_time' => date('Y-m-d H:i:s'),
+			'email' => $data['email'],
+			'password' => $pwd_hash
+		);
+		$qry = $this->db->insert('user', $user_data);
+
+		if ($this->db->affected_rows() != 1)
+			return $this->db->error()['code'] == 1062 ? 'duplicated' : FALSE;
+
+		unset($user_data['password']);
+		$user_data['id'] = $this->db->insert_id();
+		return $user_data;
+	}
 }
